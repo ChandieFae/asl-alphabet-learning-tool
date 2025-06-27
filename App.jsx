a2c762e6b2d7df83cfc92e27fbed18019d00ef95
Chandra Brown <chandie29@gmail.com>
2025-06-27 00:33:11 -0400

    Update App.jsx
    added # main app function

--- App.jsx
+++ App.jsx
@@ -1,1 +1,11 @@
- # main app
+import ASLChart from "./components/ASLChart";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <ASLChart />
+    </div>
+  );
+}
+
+export default App;
